d1ee64e5bd1bff867d02d4fb4b55c100a4014a9e
yusuf emre altun <yusufemrealtun08@gmail.com>
2023-04-08 10:44:14 +0300

    product add and update pages were designed

---
+++ add-product.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>ÜRÜN BURADA</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include 'topbar.php'; ?>
+    <div class="container">
+        <div class="row justify-content-center m-5">
+            <div class="col-md-6">
+                <form>
+                    <div class="form-group">
+                        <label for="product_image">Ürün Görseli</label>
+                        <input type="file" class="form-control-file" id="product_image">
+                    </div>
+                    <div class="form-group">
+                        <label for="product_code">Ürün Kodu</label>
+                        <input type="text" class="form-control" id="product_code">
+                    </div>
+                    <div class="form-group">
+                        <label for="product_name">Ürün Adı</label>
+                        <input type="text" class="form-control" id="product_name">
+                    </div>
+                    <div class="form-group">
+                        <label for="product_description">Ürün Açıklaması</label>
+                        <textarea class="form-control" id="product_description" rows="3"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="product_price">Ürün Fiyatı</label>
+                        <input type="number" class="form-control" id="product_price">
+                    </div>
+                    <div class="form-group">
+                        <label for="product_quantity">Ürün Adeti</label>
+                        <input type="number" class="form-control" id="product_quantity">
+                    </div>
+                    <form action="">
+                        <button type="submit" class="btn btn-info">Ürünü Ekle</button>
+                    </form>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
